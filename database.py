3371a88faac714e1f1fa01cafb8767f65cc18204
cristianndan14 <105828903+cristianndan14@users.noreply.github.com>
2023-04-16 18:57:33 +0000

    added function to load job from db with "id"

--- database.py
+++ database.py
@@ -21,3 +21,15 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(zip(column_names, row)))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {'val':id})
+    rows = result.all()
+    column_names = result.keys()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(zip(column_names, rows[0]))
+      
+  
