97b22b52a04d16f60761cac7b59d37da08df2e13
cristianndan14 <105828903+cristianndan14@users.noreply.github.com>
2023-04-17 17:22:04 +0000

    added job page and styles

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 def hello_jovian():
   jobs = load_jobs_from_db()
   return render_template('home.html',
-                         jobs=jobs,
-                        company_name='Jovian')
+                         jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
@@ -20,7 +19,9 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
-  return jsonify(job)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == "__main__":
